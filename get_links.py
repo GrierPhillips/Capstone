8ca02860ee81459934c53d0bcd2ae8d97a1445e7
Grier <GrierPhillips@gmail.com>
2016-11-10 11:52:20 -0800

    Found were site was blocking scraper. changed torrc to include most recent relay with appropriate bandwidth. Updated GolfNow class to remove cookies after each browser.get call. Note: bug in selenium does not allow phantomjs to operate without cookies, even when calling browser.enableCookies = False or seeting desired_capabilties[enableCookies] = False

--- get_links.py
+++ get_links.py
@@ -1,24 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import cPickle as pickle
-import socks
-import socket
+import time
 
-path_to_chrome = '/Users/Doedy/Documents/Galvanize/Capstone/chromedriver'
-path_to_phantomjs = '/usr/local/Cellar/phantomjs/2.1.1/bin/phantomjs'
 dcap = dict(DesiredCapabilities.PHANTOMJS)
 dcap["phantomjs.page.settings.userAgent"] = ("Chrome/15.0.87")
-# browser = webdriver.PhantomJS(executable_path = path_to_phantomjs, desired_capabilities=dcap)
 
 urls = ['https://www.golfnow.com/course-directory', 'http://www.golfadvisor.com/course-directory/']
 
+service_args = [
+    '--proxy=127.0.0.1:9050',
+    '--proxy-type=socks5',
+    ]
+
 class GolfNow(object):
     def __init__(self, url):
-        self.browser = webdriver.PhantomJS(desired_capabilities=dcap)
+        self.browser = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
         self.url = url
 
     def get_courses(self):
         self.browser.get(self.url)
+        self.browser.delete_all_cookies()
         countries = self.get_href_from_class('country-cube')
         courses = set()
         # for country in countries:
@@ -32,10 +34,13 @@ class GolfNow(object):
         #             courses += self.browser.find_element_by_class_name('featured')
         #             courses += [result.find_element_by_tag_name('a').get_attribute('href') for result in self.browser.find_elements_by_class_name('result')]
         self.browser.get(countries[0])
+        self.browser.delete_all_cookies()
         states = self.get_href_from_class('col-20')
         self.browser.get(states[0])
+        self.browser.delete_all_cookies()
         cities = self.get_href_from_class('city-cube')
         self.browser.get(cities[0])
+        self.browser.delete_all_cookies()
         courses.update(self.get_href_from_class('featured'))
         courses.update(self.get_href_from_class('result'))
         return courses
@@ -43,6 +48,10 @@ class GolfNow(object):
     def get_href_from_class(self, class_name):
         elements = self.browser.find_elements_by_class_name(class_name)
         return [element.find_element_by_tag_name('a').get_attribute('href') for element in elements]
+
+    # def get_reviews(self, course_url):
+    #     all_reviews = self.browser.find_element_by_id('ListReviews')
+        # all_reviews.find_element_by_xpath()'.//*[@id="ListReviews"]/div[1]'
 
 class GolfAdvisor(object):
     def __init__(self, url):
@@ -69,8 +78,6 @@ class GolfAdvisor(object):
         return [element.find_element_by_tag_name('a').get_attribute('href') for element in elements]
 
 if __name__ == '__main__':
-    # socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=9050)
-    # socket.socket = socks.socksocket
     gn = GolfNow(urls[0])
     courses = gn.get_courses()
     # ga = GolfAdvisor(urls[1])
