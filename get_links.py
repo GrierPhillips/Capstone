690c952184851ea81cb472e113fd3184890652d8
Grier <GrierPhillips@gmail.com>
2016-11-10 16:44:07 -0800

    started implementing logic for splitting up reviews and course info. Wrong approach. Will shift to storing entire sections in database and then parsing the information with beautiful soup later.

--- get_links.py
+++ get_links.py
@@ -1,61 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import cPickle as pickle
-import time
+from bs4 import BeautifulSoup
+import re
 
 dcap = dict(DesiredCapabilities.PHANTOMJS)
 dcap["phantomjs.page.settings.userAgent"] = ("Chrome/15.0.87")
 
-urls = ['https://www.golfnow.com/course-directory', 'http://www.golfadvisor.com/course-directory/']
+urls = ['https://www.golfnow.com/course-directory',
+        'http://www.golfadvisor.com/course-directory/']
 
 service_args = [
     '--proxy=127.0.0.1:9050',
     '--proxy-type=socks5',
     ]
 
-class GolfNow(object):
+class ReviewScraper(object):
     '''
-    Class containing methods for scraping information from the GolfNow website.
+    Base class for building specific golf review website scraping objects.
     '''
     def __init__(self, url):
-        self.browser = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
+        self.browser = webdriver.PhantomJS(desired_capabilities=dcap,
+                                           service_args=service_args)
         self.url = url
 
-    def get_courses(self):
-        '''
-        Function to crawl through all cities, states, and countries to collect complete list of all course links.
-        INPUT:
-            self: uses self.url and self.browser
-        OUTPUT:
-            courses: a set of all courses listed on the website
-        '''
-        self.browser.get(self.url)
+    def get_site(self, url):
+        self.browser.get(url)
         self.browser.delete_all_cookies()
-        countries = self.get_href_from_class('country-cube')
-        courses = set()
-        for country in countries:
-            self.browser.get(country)
-            states = self.get_href_from_class('col-20')
-            for state in states:
-                self.browser.get(state)
-                cities = [city.find_element_by_tag_name('a').get_attribute('href') for city in self.browser.find_elements_by_class_name('city-cube')]
-                for city in cities:
-                    self.browser.get(city)
-                    courses.update(self.browser.find_element_by_class_name('featured'))
-                    courses.update([result.find_element_by_tag_name('a').get_attribute('href') for result in self.browser.find_elements_by_class_name('result')])
-
-        'Test Function to return course in aruba.'
-        # self.browser.get(countries[0])
-        # self.browser.delete_all_cookies()
-        # states = self.get_href_from_class('col-20')
-        # self.browser.get(states[0])
-        # self.browser.delete_all_cookies()
-        # cities = self.get_href_from_class('city-cube')
-        # self.browser.get(cities[0])
-        # self.browser.delete_all_cookies()
-        # courses.update(self.get_href_from_class('featured'))
-        # courses.update(self.get_href_from_class('result'))
-        return courses
 
     def get_href_from_class(self, class_name):
         '''
@@ -70,21 +41,68 @@ class GolfNow(object):
             links: list of links contained within the specific class element
         '''
         elements = self.browser.find_elements_by_class_name(class_name)
-        links = [element.find_element_by_tag_name('a').get_attribute('href') for element in elements]
+        links = [element.find_element_by_tag_name('a').get_attribute('href') \
+                 for element in elements]
         return links
 
-    # def get_reviews(self, course_url):
-    #     all_reviews = self.browser.find_element_by_id('ListReviews')
-        # all_reviews.find_element_by_xpath()'.//*[@id="ListReviews"]/div[1]'
-
-class GolfAdvisor(object):
-    def __init__(self, url):
-        self.browser = webdriver.PhantomJS(desired_capabilities=dcap)
-        self.url = url
-
+class GolfNow(ReviewScraper):
+    '''
+    Class containing methods for scraping information from the GolfNow website.
+    '''
     def get_courses(self):
         '''
-        Function to crawl through all cities, states, and countries to collect complete list of all course links.
+        Function to crawl through all cities, states, and countries to collect
+        complete list of all course links.
+        INPUT:
+            self: uses self.url and self.browser
+        OUTPUT:
+            courses: a set of all courses listed on the website
+        '''
+        self.get_site(self.url)
+        countries = self.get_href_from_class('country-cube')
+        courses = set()
+        for country in countries:
+            self.get_site(country)
+            states = self.get_href_from_class('col-20')
+            for state in states:
+                self.get_site(state)
+                cities = [city.find_element_by_tag_name('a')\
+                          .get_attribute('href') for city in self.browser\
+                          .find_elements_by_class_name('city-cube')]
+                for city in cities:
+                    self.get_site(city)
+                    courses.update(self.browser\
+                                   .find_element_by_class_name('featured'))
+                    courses.update([result.find_element_by_tag_name('a')\
+                                    .get_attribute('href') for result in \
+                                    self.browser\
+                                    .find_elements_by_class_name('result')])
+
+        'Test Function to return course in aruba.'
+        # self.get_site(countries[0])
+        # states = self.get_href_from_class('col-20')
+        # self.get_site(states[0])
+        # cities = self.get_href_from_class('city-cube')
+        # self.get_site(cities[0])
+        # courses.update(self.get_href_from_class('featured'))
+        # courses.update(self.get_href_from_class('result'))
+        return courses
+
+    def get_reviews(self, course_url):
+        import pdb; pdb.set_trace()
+        self.get_site(course_url)
+        all_reviews = self.browser.find_element_by_id('ListReviews')
+        rows = all_reviews.find_elements_by_class_name('row')
+
+class GolfAdvisor(ReviewScraper):
+    '''
+    Class containing methods for scraping information from the GolfAdvisor
+    website.
+    '''
+    def get_courses(self):
+        '''
+        Function to crawl through all cities, states, and countries to collect
+        complete list of all course links.
         INPUT:
             self: uses self.url and self.browser
         OUTPUT:
@@ -108,21 +126,85 @@ class GolfAdvisor(object):
                     courses.update(self.get_href_from_class('title'))
         return courses
 
-    def get_href_from_class(self, class_name):
+    def get_reviews(self, url):
+        self.get_site(url)
+        import pdb; pdb.set_trace()
+        review = self.browser.find_element_by_id('reviewswrapper')
+        reviews = review.find_elements_by_xpath("//div[@itemprop='review']")
+        course_doc = self.get_course_info()
+        course_doc['reviews'] = {}
+        for review in reviews:
+            course_doc['reviews'].update(self.get_user_review(review))
+
+    def get_course_info(self):
         '''
-        Function to pull links from withing elements of a specific class name.
-        For example, given <div class='course'><a href=$LINK></div> the function
-        would return $LINK.
+        Create an entry for a golf course, including course stats and info
         INPUT:
-            self: uses self.browser
-            class_name: string of the class name for the element we want to find
-                links within
+            self: uses self.browser to find and return data
         OUTPUT:
-            links: list of links contained within the specific class element
+            course_doc: a dictionary containing the course stats and info
         '''
-        elements = self.browser.find_elements_by_class_name(class_name)
-        links = [element.find_element_by_tag_name('a').get_attribute('href') for element in elements]
-        return links
+        course_doc = {}
+        name = self.browser.find_element_by_xpath("//span[@itemprop='name']")\
+                                                  .get_attribute('innerHTML')
+        course_doc['name'] = name
+        atts = self.browser\
+               .find_element_by_class_name('course-essential-info-top')\
+               .find_elements_by_tag_name('li')
+        # go through attributes and convert numbers to float or int
+        for att in atts[:-1]:
+            name, val = att.get_attribute('innerHTML').split()
+            name = name[:-1]
+            if val.find('.') != -1:
+                val = float(val)
+            # fix all values to yards
+            elif 'meters' in val:
+                val = int(round(int(val[:-7]) * 1.09361329834))
+            elif 'yards' in val:
+                val = int(val[:-6])
+            course_doc[name] = val
+        # last list element is an 'a' tag with attributes for id, lat, and long
+        for att in atts[-1].get_attribute('innerHTML').strip().split()[3:6]:
+            name, val = att.replace('\"', '').split('=')
+            if val.find('.') != -1:
+                val = float(val)
+            course_doc[name] = val
+        address = self.browser.find_element_by_class_name('address')\
+                  .get_attribute('innerHTML')
+        course_doc['address'] = address
+        key_info = self.browser.find_element_by_class_name('key-info')\
+                   .find_elements_by_tag_name('div')
+        arch_div = key_info.pop(3).get_attribute('innerHTML')
+        text = BeautifulSoup(arch_div, 'html.parser').text
+        text = text.strip().split(':\n')
+        year_cleanr = re.compile('\([0-9]*\)')
+        title_cleanr = re.compile(',\s[A-Z]r.')
+        name, val = text[0], text[1].strip()
+        val = re.sub(year_cleanr, '', val)
+        val = re.sub(title_cleanr, '', val).split(',')
+        for i in xrange(len(val)):
+            val[i] = val[i].strip()
+        course_doc[name] = val
+        for key in key_info[2:]:
+            name, val = key.get_attribute('innerHTML').strip().split(': ')
+            course_doc[name] = val
+
+    def get_user_review(self, review):
+        user_review = {}
+        author = review.find_element_by_xpath(".//span[@itemprop='author']")\
+                 .get_attribute('innerHTML')
+        age = review.find_element_by_xpath(".//span[@class='context-value Age']")\
+              .get_attribute('innerHTML')
+        gen = review.find_element_by_xpath(".//span[@class='context-value Gender']")\
+              .get_attribute('innerHTML')
+        user_review['author'] = author
+        user_review['age'] = age
+        user_review['gender'] = gen
+
+
+        return course_doc
+
+
 
 if __name__ == '__main__':
     gn = GolfNow(urls[0])
