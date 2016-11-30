8635d763c61b18c9d04b7525685f69a2af8a1c6f
Grier <GrierPhillips@gmail.com>
2016-11-30 08:00:14 -0800

    minor changes

--- flask_app/ItemItemRecommender.py
+++ flask_app/ItemItemRecommender.py
@@ -54,7 +54,7 @@ class ItemItemRecommender(object):
                                             courses_rated,
                                             assume_unique=True)  # assume_unique speeds up intersection op
             relevant_courses = [np.where(courses_rated == x) for x in relevant_items]
-            print relevant_items, relevant_courses
+            print relevant_items, relevant_courses, self.item_sim_mat[course, relevant_items.sum()]
             out[course] = ratings[relevant_courses].dot(self.item_sim_mat[course, relevant_items]) / \
                 self.item_sim_mat[course, relevant_items].sum()
         cleaned_out = np.nan_to_num(out)
