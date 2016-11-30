ac4abae5b0ab269169dccc1a7f54cb1c8660e993
Grier <GrierPhillips@gmail.com>
2016-11-30 01:00:12 -0800

    removed start = False

--- flask_app/app.py
+++ flask_app/app.py
@@ -43,7 +43,6 @@ def index():
                 start = False
         except:
             print 'get_rex not started'
-            start = False
         if start:
             print 'running get_rex'
             future = executor.submit(get_rex, session['username'].lower())
