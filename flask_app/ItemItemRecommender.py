9c1db950d9381ab4619abbd1253cf28346cd375f
Grier <GrierPhillips@gmail.com>
2016-11-30 14:29:29 -0800

    hopefully fixed

--- flask_app/ItemItemRecommender.py
+++ flask_app/ItemItemRecommender.py
@@ -47,7 +47,7 @@ class ItemItemRecommender(object):
                 out[item_to_rate] = ratings[relevant_items_index].dot(self.item_sim_mat[item_to_rate, relevant_items]) / self.item_sim_mat[item_to_rate, relevant_items].sum()
             except:
                 # print ratings[relevant_items_index].dot(self.item_sim_mat[item_to_rate, relevant_items]) / self.item_sim_mat[item_to_rate, relevant_items].sum()
-                print ratings[relevant_items_index].dot(self.item_sim_mat[item_to_rate, relevant_items]), self.item_sim_mat[item_to_rate, relevant_items].sum()
+                # print ratings[relevant_items_index].dot(self.item_sim_mat[item_to_rate, relevant_items]), self.item_sim_mat[item_to_rate, relevant_items].sum()
                 print ratings[relevant_items_index], self.item_sim_mat[item_to_rate, relevant_items]
         cleaned_out = np.nan_to_num(out)
         return cleaned_out
