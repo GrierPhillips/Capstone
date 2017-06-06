ed4ac9d091cce33b049d8f9fb0faa2c939e533de
Grier Phillips <GrierPhillips@gmail.com>
2017-06-06 12:09:26 -0600

    Create __init__.py for setting up package

---
+++ GolfRecs/application/app/__init__.py
@@ -0,0 +1,14 @@
+"""Initialize the application."""
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_wtf import CSRFProtect
+
+APP = Flask(__name__)
+APP.config.from_object('config')
+LM = LoginManager()
+LM.init_app(APP)
+LM.login_view = 'login'
+CSRF = CSRFProtect(APP)
+
+from app import views  # pylint: disable=W0611; # noqa
