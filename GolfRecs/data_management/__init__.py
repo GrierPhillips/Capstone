19abfce6f9ad8fe2405e19e42990939242ce674b
Grier Phillips <GrierPhillips@gmail.com>
2017-06-12 13:55:02 -0600

    Reorganize imports in data_management package

--- GolfRecs/data_management/__init__.py
+++ GolfRecs/data_management/__init__.py
@@ -1,3 +1,7 @@
-from .utils import *
-from .data_handler import DataHandler
-from .data_collector import DataCollector
+"""Initialize data management modules."""
+
+from .utils import (check_pages, get_course_info, get_extras, get_key_info,  # noqa
+                    get_layout, get_tee_info, parse_address, parse_review,
+                    parse_user_info, renew_connection)
+from .data_handler import DataHandler  # noqa
+from .data_collector import DataCollector  # noqa
