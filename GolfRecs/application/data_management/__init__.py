27cf1456ea0e7ca654bdd4ad3ba5723b0c2b83a1
Grier Phillips <GrierPhillips@gmail.com>
2017-06-16 12:04:13 -0600

    Move data_management inside of application

--- GolfRecs/application/data_management/__init__.py
+++ GolfRecs/application/data_management/__init__.py
@@ -1,7 +1,7 @@
 """Initialize data management modules."""
 
 from .utils import (check_pages, get_course_info, get_extras, get_key_info,  # noqa
-                    get_layout, get_tee_info, parse_address, parse_review,
-                    parse_user_info, renew_connection)
+                    get_layout, get_tee_info, make_mongo_update, parse_address,
+                    parse_review, parse_user_info, renew_connection)
 from .data_handler import DataHandler  # noqa
 from .data_collector import DataCollector  # noqa
